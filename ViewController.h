8c7a314551ae4f9a70f4ab580450e1ed77c07a2d
948769029 <apple@appledeMacBook-Pro-2.local>
2018-11-21 14:05:54 +0800

    AvoidCrashDemo
    简单防止字典 数组 NSObject 崩溃代码 runtime机制更改 避免崩溃

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvoidCrashDemo
+//
+//  Created by apple on 2018/11/21.
+//  Copyright © 2018 张洪健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
